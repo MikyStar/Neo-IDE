e19055549a11a6839e9f620bd89c1e3b4d243667
Mike AUBENAS <mike.aubenas@gmail.com>
2025-11-24 18:46:33 +0100

    fix(user-command): close-other-buffers keeps nerdtree

--- lua/commands.lua
+++ lua/commands.lua
@@ -9,5 +9,20 @@ command("CloseQuickfixes", function()
 end, {})
 
 command("CloseOtherBuffers", function()
-  vim.cmd("%bdelete|edit #")
+  local current_buf = vim.api.nvim_get_current_buf()
+  local buffers = vim.api.nvim_list_bufs()
+
+  for _, buf in ipairs(buffers) do
+    -- Skip current buffer and invalid buffers
+    if buf ~= current_buf and vim.api.nvim_buf_is_valid(buf) then
+      local buftype = vim.bo[buf].buftype
+      local filetype = vim.bo[buf].filetype
+
+      -- Don't close special buffers like NvimTree, terminals, etc.
+      if buftype == "" and filetype ~= "NvimTree" then
+        pcall(vim.api.nvim_buf_delete, buf, { force = false })
+      end
+    end
+  end
 end, {})
+
