3f79bcc76e4063fd75cf57b6027a5114ed878f41
Mike AUBENAS <mike.aubenas@gmail.com>
2025-11-24 19:04:07 +0100

    fix(ts): attach lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,5 +1,13 @@
 require("nvchad.configs.lspconfig").defaults()
 
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
+
+local lspconfig = require("lspconfig")
+
+----------------------------------------
+
 local servers = {
   "html",
   "cssls",
@@ -12,6 +20,24 @@ local servers = {
   "js-debug-adapter",
   "codelldb"
 }
+
+----------------------------------------
+
+-- TypeScript/JavaScript
+lspconfig.ts_ls.setup({
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+})
+
+-- ESLint
+lspconfig.eslint.setup({
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+})
+
+----------------------------------------
 
 vim.lsp.enable(servers)
 
