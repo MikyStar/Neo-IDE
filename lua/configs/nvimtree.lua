d73ed942da180da8a26ad0a4c101eb6be45ec527
Mike AUBENAS <mike.aubenas@gmail.com>
2026-06-21 00:19:30 +0200

    fix(nvim-tree): also numerical sort

--- lua/configs/nvimtree.lua
+++ lua/configs/nvimtree.lua
@@ -2,6 +2,26 @@ local M = {}
 
 
 M = {
+  sort = {
+    sorter = function(nodes)
+      table.sort(nodes, function(a, b)
+        -- folders first
+        if a.type ~= b.type then
+          return a.type == "directory"
+        end
+
+        -- natural alphanumeric sort
+        local function to_natural(s)
+          return (s:lower():gsub("(%d+)", function(n)
+            return ("%010d"):format(tonumber(n))
+          end))
+        end
+
+        return to_natural(a.name) < to_natural(b.name)
+      end)
+    end,
+  },
+
   git = {
     enable = true,
   },
